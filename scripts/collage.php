12021ce5b6e81fdca66f9e98f1dda6c7b75fc02a
Erika Heidi <erika.christina@gmail.com>
2013-09-18 17:54:34 +0200

    update collage example

--- scripts/collage.php
+++ scripts/collage.php
@@ -13,10 +13,12 @@ header("Content-type: image/jpg");
 
 $imanee = new Imanee\Imanee($res_jpg);
 
-/* places 4 different png images on the 4 corners of the original image  */
+/* places 4 different png images on the 4 corners of the original image (placeImage method)
+and place one image on specific coordinates performing a resize (compositeImage method) */
 
 echo $imanee->placeImage($res_png1, \Imanee\Imanee::IM_POS_TOP_LEFT)
             ->placeImage($res_png2, \Imanee\Imanee::IM_POS_TOP_RIGHT)
             ->placeImage($res_png3, \Imanee\Imanee::IM_POS_BOTTOM_LEFT)
             ->placeImage($res_png4, \Imanee\Imanee::IM_POS_BOTTOM_RIGHT)
+            ->compositeImage($res_png2, 150, 150, 50, 50)
             ->output();
