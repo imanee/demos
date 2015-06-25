cc9ad06e81d2dc9771aad6d0a956366bafe98114
Tieme van Veen <tiemevanveen@hotmail.com>
2015-06-25 21:08:20 +0200

    Added Gaussian filter, renamed filter samples

---
+++ scripts/filters_gaussian.php
@@ -0,0 +1,12 @@
+<?php
+/* title: Using Filters - Color */
+/* description: This example uses the built-in <strong>color</strong> filter */
+include __DIR__ . '/../vendor/autoload.php';
+
+use Imanee\Imanee;
+
+header("Content-type: image/jpg");
+$imanee = new Imanee(__DIR__ . '/../resources/img01.jpg');
+
+echo $imanee->applyFilter('filter_gaussian', ['radius' => 5, 'sigma' => 5])
+    ->output();
